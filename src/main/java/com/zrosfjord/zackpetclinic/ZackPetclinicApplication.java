8a1d337e1f05e4e8cadd3541a341f82bdec58661
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 13:46:24 -0400

    Initial Commit

---
+++ src/main/java/com/zrosfjord/zackpetclinic/ZackPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.zrosfjord.zackpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZackPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZackPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zrosfjord/zackpetclinic/ZackPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrosfjord.zackpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZackPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
