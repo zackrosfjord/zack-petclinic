b6c880d8a4ead66c3befe6b41dce7e8f7978b5fb
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 21:44:31 -0400

    Created interfaces for Pet Clinic. Closes #5

---
+++ pet-clinic-data/src/main/java/com/zrosfjord/petclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package com.zrosfjord.petclinic.services;
+
+
+import com.zrosfjord.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
