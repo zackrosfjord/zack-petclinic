35d7993ed6e0acfc3113b5d51f46402fc41f7aa2
BuildTools <unconfigured@null.spigotmc.org>
2020-04-24 14:50:30 -0400

    Added owners page and controller. Closes #13

--- pet-clinic-web/src/main/java/com/zrosfjord/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/zrosfjord/petclinic/controllers/OwnerController.java
@@ -3,13 +3,13 @@ package com.zrosfjord.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    public String listOwners() {
+        return "owners/index";
     }
 
 }
