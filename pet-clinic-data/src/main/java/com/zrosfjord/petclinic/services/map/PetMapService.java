5180cfcebdc2edfcbcdbd3c4ea178a5e71131578
BuildTools <unconfigured@null.spigotmc.org>
2020-04-24 14:13:16 -0400

    Implemented Map Based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/zrosfjord/petclinic/services/map/PetMapService.java
@@ -0,0 +1,33 @@
+package com.zrosfjord.petclinic.services.map;
+
+import com.zrosfjord.petclinic.models.Pet;
+import com.zrosfjord.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
