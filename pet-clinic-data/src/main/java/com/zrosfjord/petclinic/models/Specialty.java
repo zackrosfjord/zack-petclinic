6f6de513bfa97f62fb09bcdded67203ac6714c83
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 15:36:19 -0400

    Added specialty and associated it with Vet

---
+++ pet-clinic-data/src/main/java/com/zrosfjord/petclinic/models/Specialty.java
@@ -0,0 +1,14 @@
+package com.zrosfjord.petclinic.models;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
